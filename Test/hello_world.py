96fcfb292d419ba6b2ab8322a6166aeb3df57575
Laursen <201703930@post.au.dk>
2022-11-14 15:57:08 +0100

    add files

---
+++ Test/hello_world.py
@@ -0,0 +1,12 @@
+import kodo
+
+
+def main():
+    """A simple hello world example."""
+
+    # Print the version of the library used
+    print(kodo.__version__)
+
+
+if __name__ == "__main__":
+    main()
